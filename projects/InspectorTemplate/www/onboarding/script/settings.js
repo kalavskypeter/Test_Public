c238d44aced3c946cbdcf8fc05e75c8587b600c7
kalavskypeter <47524961+kalavskypeter@users.noreply.github.com>
2019-05-31 12:54:38 +0200

    InspectorTemplate - 1

---
+++ projects/InspectorTemplate/www/onboarding/script/settings.js
@@ -0,0 +1,29 @@
+﻿var slides = document.getElementsByClassName("swiper-slide").length;
+var changeRatio = (slides - 1.5) / (slides - 1);
+var footerAction = document.getElementById('footerAction');
+
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    parallax: true,
+    effect: 'slide',
+    resistance: true,
+    resistanceRatio: 0,
+
+    pagination: {
+        el: '.swiper-pagination'
+    },
+
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+    },
+    on: {
+        progress: function(progress) {
+            if (progress > changeRatio) {
+                footerAction.innerHTML = "Let's go!";
+            } else {
+                footerAction.innerHTML = "Skip the tour";
+            }
+        }
+    }
+});
